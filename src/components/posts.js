d26b86e06d0db5cc3226c3437516f6f5d554a625
Richard Lenkovits <richard.lenkovits@gmail.com>
2020-07-09 17:09:38 +0200

    Extract post collection components

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,44 +1,46 @@
-import React from "react"
-import { graphql, useStaticQuery } from "gatsby"
-import PostBox from "../components/postBox"
+import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import PostBox from '../components/postBox'
 
 const Posts = () => {
-  const data = useStaticQuery(
-    graphql`
-      query {
-        allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-          edges {
-            node {
-              excerpt
-              fields {
-                slug
-              }
-              frontmatter {
-                date(formatString: "MMMM DD, YYYY")
-                title
-                description
-              }
+    const data = useStaticQuery(
+        graphql`
+            query {
+                allMarkdownRemark(
+                    sort: { fields: [frontmatter___date], order: DESC }
+                ) {
+                    edges {
+                        node {
+                            excerpt
+                            fields {
+                                slug
+                            }
+                            frontmatter {
+                                date(formatString: "MMMM DD, YYYY")
+                                title
+                                description
+                            }
+                        }
+                    }
+                }
             }
-          }
-        }
-      }
-    `
-  )
-
-  const posts = data.allMarkdownRemark.edges
-
-  return posts.map(({ node }) => {
-    const title = node.frontmatter.title || node.fields.slug
-    return (
-      <PostBox
-        slug={node.fields.slug}
-        title={title}
-        date={node.frontmatter.date}
-        description={node.frontmatter.description}
-        excerpt={node.excerpt}
-      />
+        `,
     )
-  })
+
+    const posts = data.allMarkdownRemark.edges
+
+    return posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug
+        return (
+            <PostBox
+                slug={node.fields.slug}
+                title={title}
+                date={node.frontmatter.date}
+                description={node.frontmatter.description}
+                excerpt={node.excerpt}
+            />
+        )
+    })
 }
 
 export default Posts
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,28 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React from 'react'
+import { graphql } from 'gatsby'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import Posts from "../components/posts"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Posts from '../components/posts'
 
 const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-  return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
-      <Posts />
-    </Layout>
-  )
+    const siteTitle = data.site.siteMetadata.title
+    return (
+        <Layout location={location} title={siteTitle}>
+            <SEO title='All posts' />
+            <Posts />
+        </Layout>
+    )
 }
 
 export default BlogIndex
 
 export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
+    query {
+        site {
+            siteMetadata {
+                title
+            }
+        }
     }
-  }
 `
